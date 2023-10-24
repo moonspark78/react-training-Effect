6b9117577a6f834c5c0c956a8d5e1aab0d4c9da4
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-10-25 01:00:00 +0200

    :safety_vest: :safety_vest: :safety_vest:  :art:

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import "../App.css"
 
 export const Counter = () => {
+    const [count, setCount] = useState (0)
+
+
+    useEffect(() =>{
+      console.log("The count is: ", count);
+  
+    },[count])
+
   return (
-    <div>Counter</div>
+    <div>
+        <h1>Count: {count}</h1>
+        <button className='toto ' onClick={() => setCount(count - 1 ) }> 
+          Décrémente
+        </button>
+        <button className='toto' onClick={() => setCount(count + 1 )}>
+          Imcrémente
+        </button>
+    </div>
   )
 }
